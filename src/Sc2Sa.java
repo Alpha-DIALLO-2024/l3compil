b873ef21ca1e78cbfb2d72013bf9b6eaa5c61f23
Cyrille Siouffi <cyrille.siouffi@gmail.com>
2021-02-05 17:44:33 +0100

    Refonte class Sc2Sa

--- src/Sc2Sa.java
+++ src/Sc2Sa.java
@@ -1,17 +1,44 @@
+import sa.SaDecFonc;
+import sa.SaDecVar;
+import sa.SaExp;
+import sa.SaExpAdd;
 import sa.SaNode;
+import sa.SaProg;
 import sc.analysis.DepthFirstAdapter;
 import sc.node.AAdditionAdsous;
+import sc.node.ADecvar;
 
 public class Sc2Sa extends DepthFirstAdapter {
     private SaNode returnValue;
-    
+
+    @Override
+    public void caseADecvar(ADecvar node)
+    {
+        String text = null;
+        inADecvar(node);
+        if(node.getType() != null)
+        {
+            node.getType().apply(this);
+        }
+        if(node.getIdentif() != null)
+        {
+            text = node.getIdentif().getText();
+            node.getIdentif().apply(this);
+        }
+        this.returnValue = new SaDecVar(text);
+        outADecvar(node);
+    }
+
     @Override
     public void caseAAdditionAdsous(AAdditionAdsous node)
     {
+        SaExp op1 = null;
+        SaExp op2 = null;
         inAAdditionAdsous(node);
         if(node.getAdsous() != null)
         {
             node.getAdsous().apply(this);
+            op1 = (SaExp) this.returnValue;
         }
         if(node.getPlus() != null)
         {
@@ -20,8 +47,10 @@ public class Sc2Sa extends DepthFirstAdapter {
         if(node.getMultdiv() != null)
         {
             node.getMultdiv().apply(this);
+            op2 = (SaExp) this.returnValue;
         }
         outAAdditionAdsous(node);
+        this.returnValue = new SaExpAdd(op1, op2);
     }
-    
+
 }
