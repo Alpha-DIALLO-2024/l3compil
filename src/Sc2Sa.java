11c1d5225590f93491856f6a613438c279ac8888
alfa <mamadou-alpha.DIALLO@etu.univ-amu.fr>
2021-02-05 17:47:40 +0100

    caseAAddtion

--- src/Sc2Sa.java
+++ src/Sc2Sa.java
@@ -1,6 +1,10 @@
+import sa.SaExp;
+import sa.SaExpAdd;
 import sa.SaNode;
+import sa.SaProg;
 import sc.analysis.DepthFirstAdapter;
 import sc.node.AAdditionAdsous;
+import sc.node.AProgramme;
 
 public class Sc2Sa extends DepthFirstAdapter {
     private SaNode returnValue;
@@ -8,10 +12,15 @@ public class Sc2Sa extends DepthFirstAdapter {
     @Override
     public void caseAAdditionAdsous(AAdditionAdsous node)
     {
+
+        SaExp op1 =null;
+        SaExp op2 =null;
+
         inAAdditionAdsous(node);
         if(node.getAdsous() != null)
         {
             node.getAdsous().apply(this);
+            op1 = (SaExp)this.returnValue;
         }
         if(node.getPlus() != null)
         {
@@ -20,8 +29,11 @@ public class Sc2Sa extends DepthFirstAdapter {
         if(node.getMultdiv() != null)
         {
             node.getMultdiv().apply(this);
+            op2 = (SaExp)this.returnValue;
         }
+        this.returnValue =new SaExpAdd(op1, op2);
         outAAdditionAdsous(node);
     }
+
     
 }
