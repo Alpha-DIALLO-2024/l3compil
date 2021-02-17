bca3d79988b56ebc63936236b551860cb413d339
alfa <mamadou-alpha.DIALLO@etu.univ-amu.fr>
2021-02-17 20:42:47 +0100

    methode visit(SaDecVar node) mais à ameliorer

--- src/Sa2Ts.java
+++ src/Sa2Ts.java
@@ -1,10 +1,51 @@
 import sa.*;
+import ts.Ts;
+
+import java.time.format.TextStyle;
 
 public class Sa2Ts extends SaDepthFirstVisitor <Void> {
+
+    private Ts tableGlobal;
+    private Ts tableLocal;
+    enum Context{LOCAL, GLOBAL, PARAM} //PARAM => paramettre
+    private  Context context;
+
+    public Sa2Ts(SaNode root) {
+        tableGlobal = new Ts();
+        tableLocal =  null;
+        context = Context.GLOBAL;
+        root.accept(this);
+    }
 
     public Void visit(SaDecVar node){
 
         defaultIn(node);
+
+        if(node.tsItem.taille > 1){
+            System.out.println("Erreur : "+node+" est un tableau");
+        }
+
+        if (!node.tsItem.isParam) {
+            System.out.println("Erreur : "+node+" est ni une variable locale , ni une variable globale, ni un paramettre");
+        }
+
+        if (node.tsItem.portee.getTableLocale(node.getNom()) != null) {
+            if (tableLocal.variables.containsKey(node.getNom())){
+                System.out.println("Erreur : "+node+" existe déjà");
+            }
+
+            tableLocal.variables.put(node.getNom(), node.tsItem);
+        }
+
+
+        if (node.tsItem.portee.getTableLocale(node.getNom()) == null){
+            if (tableGlobal.variables.containsKey(node.getNom())){
+                System.out.println("Erreur : "+node+" existe déjà");
+            }
+
+            tableGlobal.variables.put(node.getNom(), node.tsItem);
+        }
+
         defaultOut(node);
         return null;
 
